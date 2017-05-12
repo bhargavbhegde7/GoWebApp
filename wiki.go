0fff97f729a298448c341d7a9e860d0900fb5571
bhargav <bhargavbhegde7@gmail.com>
2017-05-13 00:45:02 +0530

    refactoring

--- wiki.go
+++ wiki.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	//"fmt"
+	"errors"
+	"html/template"
 	"io/ioutil"
 	"net/http"
-	"html/template"
-	"errors"
 )
 
 type Page struct {
@@ -19,9 +18,9 @@ func (p *Page) save() error {
 }
 
 func loadPage(title string) (*Page, error) {
-	
+
 	if len(title) == 0 {
-		return nil, errors.New("Not defined")
+		return nil, errors.New("view title is empty")
 	}
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -32,64 +31,70 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    t, err := template.ParseFiles(tmpl + ".html")
-	
+	t, err := template.ParseFiles(tmpl + ".html")
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	
+
 	err = t.Execute(w, p)
-	
+
 	if err != nil {
-		http.Error(w,err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-    
+
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, err := loadPage(title)
-	
-	if err != nil && len(title) > 0 {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}else {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	title := r.URL.Path[len("/view/"):]
+
+	if len(title) == 0 {
+		http.Error(w, "view title is empty", http.StatusInternalServerError)
 		return
 	}
-	renderTemplate(w, "view", p)
+
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	} else {
+		renderTemplate(w, "view", p)
+	}
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
-    p, err := loadPage(title)
-    if err != nil && len(title) > 0{
-        p = &Page{Title: title}
-    } else {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	title := r.URL.Path[len("/edit/"):]
+
+	if len(title) == 0 {
+		http.Error(w, "view title is empty", http.StatusInternalServerError)
 		return
 	}
-	
+
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
 	renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-	
-	if err!= nil {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-    http.HandleFunc("/save/", saveHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
 }
