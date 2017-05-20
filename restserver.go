0cd44f7c13651a3d9495d49360b752fb980e5706
Bhargava Balachandra Hegde <bhargavbhegde7@gmail.com>
2017-05-20 12:53:36 +0530

    sign up URL added

--- restserver.go
+++ restserver.go
@@ -1,31 +1,40 @@
 package main
- 
+
 import (
-    "log"
-    "net/http"
-	"html/template"
 	"fmt"
- 
-    "github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 )
 
 var cookieHandler = securecookie.New(
-    securecookie.GenerateRandomKey(64),
-    securecookie.GenerateRandomKey(32))
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
 
 var store = sessions.NewCookieStore([]byte("something-very-secret"))
- 
+
 type User struct {
-    UserName string 
+	UserName string
 }
 
 type ErrorMessage struct {
 	Message string
 }
- 
+
+//GetLoginEndpoint . . .
 func GetLoginEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	m := ErrorMessage{}
+
+	username := getUserName(req)
+	if username != "" {
+		fmt.Println("user exists")
+		m = ErrorMessage{"Already logged in as " + username}
+	}
 
 	t, err := template.ParseFiles("login.html")
 
@@ -34,60 +43,89 @@ func GetLoginEndpoint(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	err = t.Execute(w, nil)
+	err = t.Execute(w, m)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-} 
- 
-func setSession(username string, response http.ResponseWriter) {
-    value := map[string]string{
-        "username": username,
-    }
-    if encoded, err := cookieHandler.Encode("session", value); err == nil {
-        cookie := &http.Cookie{
-            Name:  "session",
-            Value: encoded,
-            Path:  "/",
-        }
-        http.SetCookie(response, cookie)
-    }
 }
 
-//this blog right here made it work. 
+//GetSignupEndpoint . . .
+func GetSignupEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	m := ErrorMessage{}
+
+	username := getUserName(req)
+	if username != "" {
+		fmt.Println("user exists")
+		m = ErrorMessage{"Already logged in as " + username}
+	}
+
+	t, err := template.ParseFiles("signup.html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, m)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func setSession(username string, response http.ResponseWriter) {
+	value := map[string]string{
+		"username": username,
+	}
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
+		http.SetCookie(response, cookie)
+	}
+}
+
+//this blog right here made it work.
 //don't understand the code fully
 //	https://mschoebel.info/2014/03/09/snippet-golang-webapp-login-logout/
 
-
+//LoginEndpoint . . .
 func LoginEndpoint(w http.ResponseWriter, req *http.Request) {
 
-    username := req.FormValue("username")
+	username := req.FormValue("username")
 	passwd := req.FormValue("passwd")
-	
+
 	//check if the uname and password match
-	if username == "bhargav" && passwd == "bhargav"{
+	if username == "bhargav" && passwd == "bhargav" {
+
+		//check if session exists, get the session with uname
+		//start a new session if doesn't exist
+
+		//TODO or not to do??
+		//clearSession(w)
 		
+		setSession(username, w)
+
+		//TODO send the user to "/home" with all these incoming data
+		http.Redirect(w, req, "/home", http.StatusFound)
+		return
+	} else if username == "bhargav2" && passwd == "bhargav2" {
+
 		//check if session exists, get the session with uname
 		//start a new session if doesn't exist
 		setSession(username, w)
-		
+
 		//TODO send the user to "/home" with all these incoming data
 		http.Redirect(w, req, "/home", http.StatusFound)
-			return
-	}else if username == "bhargav2" && passwd == "bhargav2"{
-		
-		//check if session exists, get the session with uname
-		//start a new session if doesn't exist
-		setSession(username, w)
-		
-		//TODO send the user to "/home" with all these incoming data
-		http.Redirect(w, req, "/home", http.StatusFound)
-			return
-	}else{
-	
-		m := ErrorMessage{Message:"wrong credentials"}
-	
+		return
+	} else {
+
+		m := ErrorMessage{Message: "wrong credentials"}
+
 		t, err := template.ParseFiles("login.html")
 
 		if err != nil {
@@ -99,51 +137,86 @@ func LoginEndpoint(w http.ResponseWriter, req *http.Request) {
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}	
-	}	
+		}
+	}
+}
+
+//SignupEndpoint . . .
+func SignupEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	var m ErrorMessage
+
+	username := req.FormValue("username")
+	passwd1 := req.FormValue("passwd1")
+	passwd2 := req.FormValue("passwd2")
+
+	//TODO create a validation method
+	if username != "" && passwd1 != "" && passwd1 == passwd2 {
+		fmt.Println("username : " + username + " , password : " + passwd1)
+		http.Redirect(w, req, "/home", http.StatusFound)
+		return
+	}
+
+	m = ErrorMessage{Message: "error : enter proper username and password"}
+
+	t, err := template.ParseFiles("signup.html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, m)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func clearSession(response http.ResponseWriter) {
-    cookie := &http.Cookie{
-        Name:   "session",
-        Value:  "",
-        Path:   "/",
-        MaxAge: -1,
-    }
-    http.SetCookie(response, cookie)
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
 }
 
+//LogoutHandler . . .
 func LogoutHandler(response http.ResponseWriter, request *http.Request) {
-    clearSession(response)
-    http.Redirect(response, request, "/", 302)
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
 }
 
+//GetIndexEndpoint . . .
 func GetIndexEndpoint(w http.ResponseWriter, req *http.Request) {
-    fmt.Println("inside index handler")
+	fmt.Println("inside index handler")
 	http.Redirect(w, req, "/home", http.StatusFound)
-		return 
+	return
 }
 
 func getUserName(request *http.Request) (username string) {
-    if cookie, err := request.Cookie("session"); err == nil {
-        cookieValue := make(map[string]string)
-        if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-            username = cookieValue["username"]
-        }
-    }
-    return username
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			username = cookieValue["username"]
+		}
+	}
+	return username
 }
 
+//GetHomeEndpoint this is to trick the ide to think there is a sensible comment here. I don't write comments at all. I am a dick
 func GetHomeEndpoint(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Println("inside home handler")
-	
+
 	username := getUserName(req)
-    if username != "" {
+	if username != "" {
 		fmt.Println("username exists")
-			
-		u := User{UserName:username}
-		
+
+		u := User{UserName: username}
+
 		t, err := template.ParseFiles("home.html")
 
 		if err != nil {
@@ -158,20 +231,20 @@ func GetHomeEndpoint(w http.ResponseWriter, req *http.Request) {
 		}
 	} else {
 		fmt.Println("username doesn't exist")
-        http.Redirect(w, req, "/login", 302)
-    }
+		http.Redirect(w, req, "/login", 302)
+	}
 }
 
 func main() {
-    router := mux.NewRouter()    
-	
+	router := mux.NewRouter()
+
 	router.HandleFunc("/", GetIndexEndpoint).Methods("GET")
 	router.HandleFunc("/home", GetHomeEndpoint).Methods("GET")
-    router.HandleFunc("/login", GetLoginEndpoint).Methods("GET")
+	router.HandleFunc("/login", GetLoginEndpoint).Methods("GET")
 	router.HandleFunc("/login", LoginEndpoint).Methods("POST")
 	router.HandleFunc("/logout", LogoutHandler).Methods("GET")
-	//router.HandleFunc("/signup", GetSignupEndpoint).Methods("GET")
-	//router.HandleFunc("/signup", SignupEndpoint).Methods("POST")
-	
-    log.Fatal(http.ListenAndServe(":12345", router))
+	router.HandleFunc("/signup", GetSignupEndpoint).Methods("GET")
+	router.HandleFunc("/signup", SignupEndpoint).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":12345", router))
 }
